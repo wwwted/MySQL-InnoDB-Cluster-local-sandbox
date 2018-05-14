a36144493b1a8fc907d04f67e4b1ac7e2511adf2
wwwted <ted.wennmark@gmail.com>
2018-05-14 14:15:11 +0200

    exec flag on scripts on new download.sh

---
+++ scripts/download.sh
@@ -0,0 +1,3 @@
+wget https://dev.mysql.com/get/Downloads/MySQL-Shell/mysql-shell-8.0.11-linux-glibc2.12-x86-64bit.tar.gz
+wget https://dev.mysql.com/get/Downloads/MySQL-Router/mysql-router-8.0.11-linux-glibc2.12-x86-64bit.tar.gz
+wget https://dev.mysql.com/get/Downloads/MySQL-8.0/mysql-8.0.11-linux-glibc2.12-x86_64.tar.gz
